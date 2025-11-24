62fa59f14f4589c7cd1f6eb414cd6227cd098b67
Ahmad Faisal <ahmad100.fa100@gmail.com>
2025-11-24 12:36:57 +0300

    Add project files.

---
+++ FinanceApp/Models/Expens.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace FinanceApp.Models
+{
+    public class Expens
+    {
+        public int Id { get; set; }
+        
+        [Required (ErrorMessage ="Enter the description please")] 
+        public string Description { get; set; } = null;
+        
+        [Required]
+        [Range(0.01 , double.MaxValue, ErrorMessage = "amount must be highr than 0.00")]
+        public double Amount { get; set; }
+        
+        [Required]
+        public string Catogry { get; set; } = null;
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
